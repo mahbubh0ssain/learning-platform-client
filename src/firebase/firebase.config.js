51238dc85a3323e4acb4fb5f35b97215e54ec4d9
mahbubh0ssain <mahbubju48@gmail.com>
2022-10-25 01:28:19 +0600

    projcect initialized

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDu_NMOd00LUwHaTrFMb7-rQnM2HQBZOQw",
+  authDomain: "learn-shine-96053.firebaseapp.com",
+  projectId: "learn-shine-96053",
+  storageBucket: "learn-shine-96053.appspot.com",
+  messagingSenderId: "560100516422",
+  appId: "1:560100516422:web:92ffcce7b9764f45252047",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
